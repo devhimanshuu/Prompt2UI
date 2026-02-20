0d6ef8c2caf216c1554b6dc04e442b5b0a299edb
devhimanshuu <devhimanshuu@gmail.com>
2026-02-20 13:37:59 +0530

    feat: Add Inngest API routes and enhance Clerk middleware with public route protection.

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,8 +1,8 @@
 import { serve } from "inngest/next";
-import { inngest } from "../../../inngest/client";
-import { helloWorld } from "../../../inngest/functions/helloWorld";
-import { generateScreens } from "../../../inngest/functions/generateScreens";
-import { regenerateFrame } from "../../../inngest/functions/regenerateFrame";
+import { inngest } from "@/inngest/client";
+import { helloWorld } from "@/inngest/functions/helloWorld";
+import { generateScreens } from "@/inngest/functions/generateScreens";
+import { regenerateFrame } from "@/inngest/functions/regenerateFrame";
 
 export const maxDuration = 60;
 
